8b39449d0de9bf55a30c809871fb89867a359be5
HiralChothani <hiralchothani5@gmail.com>
2023-03-16 15:29:09 +0000

    Java-week-1-Homework java programs - wednesday Homework

---
+++ src/homework_week_5/Programme_5_Triangle.java
@@ -0,0 +1,21 @@
+package homework_week_5;
+
+/**
+ * 5. Write a Java Program to print below ttriangle
+ *           *
+ *          * *
+ *         * * *
+ *        * * * *
+ *       * * * * *
+ *      * * * * * *
+ */
+public class Programme_5_Triangle {
+    public static void main(String[] args) {
+        System.out.println("      *      ");
+        System.out.println("     * *     ");
+        System.out.println("    * * *    ");
+        System.out.println("   * * * *   ");
+        System.out.println("  * * * * *  ");
+        System.out.println(" * * * * * * ");
+    }
+}
